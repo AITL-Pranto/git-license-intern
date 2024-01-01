99df878b23432a544a31916f1bdf6afd8912d157
Rahat Morshed Nabil <imrmnabil@gmail.com>
2024-01-01 14:22:44 +0600

    deleted:    resources/views/backend/pages/ward/ward_form.blade.php 	deleted:    resources/views/backend/pages/ward/ward_modify.blade.php 	modified:   routes/admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -70,10 +70,6 @@ Route::group(['prefix' => 'admin'], function () {
         //manage wards
         Route::get('wards', [ManageWardController::class, 'viewWards'])->name('viewWards');
         Route::get('/manage-ward/delete/{id}', [ManageWardController::class, 'deleteWard']);
-        Route::get('/manage-ward/add', [ManageWardController::class, 'addWard'])->name('adminAddWard');
-        Route::post('/manage-ward/add', [ManageWardController::class, 'addWardSubmit'])->name('adminAddWardSubmit');
-        Route::get('/manage-ward/modify/{id}', [ManageWardController::class, 'modifyWard'])->name('adminModifyWard');
-        Route::post('/manage-ward/modify/{id}', [ManageWardController::class, 'modifyWardSubmit'])->name('adminModifyWardSubmit');
         Route::post('/save-ward', [ManageWardController::class, 'wardSave']);
         //manage wards
 
